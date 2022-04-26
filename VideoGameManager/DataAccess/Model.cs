32ac91aaf50090355208c28181e5d492f74f1626
EvilWeasel <38261180+EvilWeasel@users.noreply.github.com>
2022-04-26 11:54:52 +0200

    Add project files.

---
+++ VideoGameManager/DataAccess/Model.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace VideoGameManager.DataAccess
+{
+    public class GameGenre
+    {
+        // Primary Key - Datenbank
+        public int ID { get; set; }
+        [MaxLength(150)]
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public List<Game> Games { get; set; }
+    }
+
+    public class Game
+    {
+        public int ID { get; set; }
+        [MaxLength(150)]
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public int PersonalRating { get; set; }
+        // One-to-Many-Relationship
+        public GameGenre Genre { get; set; }
+    }
+}
